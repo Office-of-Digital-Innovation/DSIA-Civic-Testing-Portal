f713e0a64047a75bb045a936c8455682af74aca7
LaJuan <8461536+madddio@users.noreply.github.com>
2018-07-15 00:41:04 -0700

    Commit baseline before adding Catalina's updates.

--- app/Controllers/FakeTestersController.cs
+++ app/Controllers/FakeTestersController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,144 +9,145 @@ using app.Models;
 
 namespace app.Controllers
 {
-    public class FakeTestersController : Controller
+  public class FakeTestersController : Controller
+  {
+    private readonly FusionAppContext _context;
+
+    public FakeTestersController(FusionAppContext context)
     {
-        private readonly FusionAppContext _context;
-
-        public FakeTestersController(FusionAppContext context)
-        {
-            _context = context;
-        }
-
-        // GET: FakeTesters
-        public async Task<IActionResult> Index()
-        {
-            return View(await _context.FakeTesters.ToListAsync());
-        }
-
-        // GET: FakeTesters/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var fakeTesters = await _context.FakeTesters
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (fakeTesters == null)
-            {
-                return NotFound();
-            }
-
-            return View(fakeTesters);
-        }
-
-        // GET: FakeTesters/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: FakeTesters/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,Email,City,Zipcode,Phone,Age,Gender,IsConfirmed,NoOfTests,PointsEarned")] FakeTesters fakeTesters)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(fakeTesters);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(fakeTesters);
-        }
-
-        // GET: FakeTesters/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var fakeTesters = await _context.FakeTesters.FindAsync(id);
-            if (fakeTesters == null)
-            {
-                return NotFound();
-            }
-            return View(fakeTesters);
-        }
-
-        // POST: FakeTesters/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName,Email,City,Zipcode,Phone,Age,Gender,IsConfirmed,NoOfTests,PointsEarned")] FakeTesters fakeTesters)
-        {
-            if (id != fakeTesters.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(fakeTesters);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!FakeTestersExists(fakeTesters.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(fakeTesters);
-        }
-
-        // GET: FakeTesters/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var fakeTesters = await _context.FakeTesters
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (fakeTesters == null)
-            {
-                return NotFound();
-            }
-
-            return View(fakeTesters);
-        }
-
-        // POST: FakeTesters/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var fakeTesters = await _context.FakeTesters.FindAsync(id);
-            _context.FakeTesters.Remove(fakeTesters);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool FakeTestersExists(int id)
-        {
-            return _context.FakeTesters.Any(e => e.Id == id);
-        }
+      _context = context;
     }
+
+    [Route("Api/[controller]")]
+    // GET: FakeTesters
+    public async Task<IActionResult> Index()
+    {
+      return View(await _context.FakeTesters.ToListAsync());
+    }
+
+    // GET: FakeTesters/Details/5
+    public async Task<IActionResult> Details(int? id)
+    {
+      if (id == null)
+      {
+        return NotFound();
+      }
+
+      var fakeTesters = await _context.FakeTesters
+          .FirstOrDefaultAsync(m => m.Id == id);
+      if (fakeTesters == null)
+      {
+        return NotFound();
+      }
+
+      return View(fakeTesters);
+    }
+
+    // GET: FakeTesters/Create
+    public IActionResult Create()
+    {
+      return View();
+    }
+
+    // POST: FakeTesters/Create
+    // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+    // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,Email,City,Zipcode,Phone,Age,Gender,IsConfirmed,NoOfTests,PointsEarned")] FakeTesters fakeTesters)
+    {
+      if (ModelState.IsValid)
+      {
+        _context.Add(fakeTesters);
+        await _context.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
+      }
+      return View(fakeTesters);
+    }
+
+    // GET: FakeTesters/Edit/5
+    public async Task<IActionResult> Edit(int? id)
+    {
+      if (id == null)
+      {
+        return NotFound();
+      }
+
+      var fakeTesters = await _context.FakeTesters.FindAsync(id);
+      if (fakeTesters == null)
+      {
+        return NotFound();
+      }
+      return View(fakeTesters);
+    }
+
+    // POST: FakeTesters/Edit/5
+    // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+    // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName,Email,City,Zipcode,Phone,Age,Gender,IsConfirmed,NoOfTests,PointsEarned")] FakeTesters fakeTesters)
+    {
+      if (id != fakeTesters.Id)
+      {
+        return NotFound();
+      }
+
+      if (ModelState.IsValid)
+      {
+        try
+        {
+          _context.Update(fakeTesters);
+          await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+          if (!FakeTestersExists(fakeTesters.Id))
+          {
+            return NotFound();
+          }
+          else
+          {
+            throw;
+          }
+        }
+        return RedirectToAction(nameof(Index));
+      }
+      return View(fakeTesters);
+    }
+
+    // GET: FakeTesters/Delete/5
+    public async Task<IActionResult> Delete(int? id)
+    {
+      if (id == null)
+      {
+        return NotFound();
+      }
+
+      var fakeTesters = await _context.FakeTesters
+          .FirstOrDefaultAsync(m => m.Id == id);
+      if (fakeTesters == null)
+      {
+        return NotFound();
+      }
+
+      return View(fakeTesters);
+    }
+
+    // POST: FakeTesters/Delete/5
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+      var fakeTesters = await _context.FakeTesters.FindAsync(id);
+      _context.FakeTesters.Remove(fakeTesters);
+      await _context.SaveChangesAsync();
+      return RedirectToAction(nameof(Index));
+    }
+
+    private bool FakeTestersExists(int id)
+    {
+      return _context.FakeTesters.Any(e => e.Id == id);
+    }
+  }
 }
--- app/Controllers/HomeController.cs
+++ app/Controllers/HomeController.cs
@@ -8,12 +8,12 @@ using app.Models;
 
 namespace app.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
+  {
+    public IActionResult Index()
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+      return View();
+    }
 
     public IActionResult GetInvolved()
     {
@@ -48,43 +48,43 @@ namespace app.Controllers
     {
       return View();
     }
-    
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
 
-            return View();
-        }
+    public IActionResult About()
+    {
+      ViewData["Message"] = "Your application description page.";
 
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        public IActionResult Condition()
-        {
-            return View();
-        }
-
-        public IActionResult Help()
-        {
-            ViewData["Message"] = "New page.";
-
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+      return View();
     }
+
+    public IActionResult Contact()
+    {
+      ViewData["Message"] = "Your contact page.";
+
+      return View();
+    }
+
+    public IActionResult Privacy()
+    {
+      return View();
+    }
+
+    public IActionResult Condition()
+    {
+      return View();
+    }
+
+    public IActionResult Help()
+    {
+      ViewData["Message"] = "New page.";
+
+      return View();
+    }
+    
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+  }
 }
