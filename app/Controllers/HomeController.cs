697cce68acd598bbbd28b5c456d47bf479fd2c30
Jake N <jake@Jakes-MBP.local>
2018-07-17 19:58:29 -0700

    Updates Koji and I made

--- app/Controllers/HomeController.cs
+++ app/Controllers/HomeController.cs
@@ -72,20 +72,14 @@ namespace app.Controllers
       return View();
     }
 
-    public IActionResult HowItWorks()
-    {
-      return View();
-    }
+   
 
     public IActionResult MoreNumbers()
     {
       return View();
     }
 
-    public IActionResult ChallengeIdea()
-    {
-      return View();
-    }
+   
     public IActionResult Tests2()
     {
       return View();
